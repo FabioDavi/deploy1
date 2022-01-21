4269bc421e6a2c32908406f27eb6706a46892145
Dev-js <59389855+FabioDavi@users.noreply.github.com>
2022-01-21 19:48:27 -0300

    ejs enabled

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get("/", function(req, res){
-    res.send(`aplicação rodando na porta ${port}`)
-})
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/home', {
+        mascots: mascots,
+        tagline: tagline
+    });
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
 
 app.listen(port, ()=>{
     console.info('rodando')
