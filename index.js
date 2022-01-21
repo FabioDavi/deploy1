5d15ec04005cb9b223588c91686aedd9926085de
Dev-js <59389855+FabioDavi@users.noreply.github.com>
2022-01-21 20:12:02 -0300

    ejs changes - new page

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ app.get('/', function(req, res) {
 
 // about page
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/about', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
 
